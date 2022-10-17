fa3fd13f75c373130fe029fa51834fe3450d3ec4
Phil Vigus <phil.vigus@devoteam.com>
2022-10-17 16:58:37 +0200

    Transfer changes from other project

--- src/main/java/com/philvigus/AbstractBaseEntityFactory.java
+++ src/main/java/com/philvigus/AbstractBaseEntityFactory.java
@@ -1,17 +1,116 @@
 package com.philvigus;
 
+import com.philvigus.exceptions.EntityFactoryException;
+import net.datafaker.Faker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.apache.commons.beanutils.BeanUtils.setProperty;
 
 public abstract class AbstractBaseEntityFactory<T> {
-  protected final Class<T> entityClass;
+    protected final Class<T> entityClass;
 
-  protected AbstractBaseEntityFactory(final Class<T> entityClass) {
-    this.entityClass = entityClass;
-  }
+    protected final Faker faker;
 
-  public T make()
-      throws NoSuchMethodException, InvocationTargetException, InstantiationException,
-          IllegalAccessException {
-    return entityClass.getDeclaredConstructor().newInstance();
-  }
+    protected final JpaRepository<T, Long> repository;
+
+    protected Map<String, Object> customAttributes;
+
+    protected AbstractBaseEntityFactory(final Class<T> entityClass, final JpaRepository<T, Long> repository) {
+        this.entityClass = entityClass;
+        this.faker = new Faker();
+        this.repository = repository;
+        this.customAttributes = new ConcurrentHashMap<>();
+    }
+
+    public List<T> create(final int copies) throws EntityFactoryException {
+        if (copies < 1) {
+            throw new IllegalArgumentException("copies must be greater than 0");
+        }
+
+        final List<T> entities = new ArrayList<>(copies);
+
+        for (int i = 0; i < copies; i++) {
+            final T entity = getEntityWithAttributesSet(customAttributes);
+            entities.add(repository.save(entity));
+        }
+
+        return entities;
+    }
+
+    public T create() throws EntityFactoryException {
+        final T entity = getEntityWithAttributesSet(customAttributes);
+
+        return repository.save(entity);
+    }
+
+    public List<T> make(final int copies) throws EntityFactoryException {
+        if (copies < 1) {
+            throw new IllegalArgumentException("copies must be greater than 0");
+        }
+
+        final List<T> entities = new ArrayList<>(copies);
+
+        for (int i = 0; i < copies; i++) {
+            final T entity = getEntityWithAttributesSet(customAttributes);
+            entities.add(entity);
+        }
+
+        return entities;
+    }
+
+    public T make() throws EntityFactoryException {
+        return getEntityWithAttributesSet(customAttributes);
+    }
+
+    public AbstractBaseEntityFactory<T> withAttributes(final Map<String, Object> customAttributes) {
+        this.customAttributes = customAttributes;
+
+        return this;
+    }
+
+    protected T getEntityWithAttributesSet(final Map<String, Object> customAttributes)
+            throws EntityFactoryException {
+        final T entity;
+
+        try {
+            entity = entityClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException
+                 | IllegalAccessException
+                 | IllegalArgumentException
+                 | InvocationTargetException
+                 | NoSuchMethodException e) {
+            throw new EntityFactoryException(String.format("Unable to create entity of type %s", entityClass), e);
+        }
+
+        getCombinedAttributes(customAttributes)
+                .forEach(
+                        (name, value) -> {
+                            try {
+                                setProperty(entity, name, value);
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                throw new EntityFactoryException(
+                                        String.format(
+                                                "Unable to set property %s to %s on entity of type %s", name, value, entityClass),
+                                        e);
+                            }
+                        });
+
+        return entity;
+    }
+
+    protected Map<String, Object> getCombinedAttributes(final Map<String, Object> customAttributes) {
+        final Map<String, Object> defaultAttributes = defaultAttributes();
+
+        defaultAttributes.putAll(customAttributes);
+
+        return defaultAttributes;
+    }
+
+    protected abstract Map<String, Object> defaultAttributes();
 }
--- src/test/java/com/philvigus/fixtures/TestFactory.java
+++ src/test/java/com/philvigus/fixtures/TestFactory.java
@@ -1,9 +1,17 @@
 package com.philvigus.fixtures;
 
 import com.philvigus.AbstractBaseEntityFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Map;
 
 public class TestFactory extends AbstractBaseEntityFactory<TestEntity> {
-  public TestFactory() {
-    super(TestEntity.class);
-  }
+    public TestFactory(final JpaRepository<TestEntity, Long> jpaRepository) {
+        super(TestEntity.class, jpaRepository);
+    }
+
+    @Override
+    protected Map<String, Object> defaultAttributes() {
+        return null;
+    }
 }
